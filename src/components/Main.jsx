5c3ebedfca84d90f2ff94afd6a8f82ecda87aa19
Akash9698 <senthilsenthil98832@gmail.com>
2024-02-07 18:08:20 +0530

    done

---
+++ src/components/Main.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Main = ({imageData}) => {
+  return (
+    <div>
+    {
+      imageData.map(image=>{
+        return(
+          <div key={image.id} className="column">
+            <img src={image.img} alt=""></img>
+          </div>
+        )
+      })
+    }
+  </div>
+  )
+}
+
+export default Main
